6802bb8a098beedff03fecded4915a3b0ab74094
msbxii <msbxii@case.edu>
2011-11-22 10:36:02 -0500

    starting setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+__install () {
+	hash 'apt-get' && echo 'I need administrator priveliges to install '\
+		"$1" && sudo apt-get install "$1" && return
+	hash 'yum' && echo 'I need administrator priveliges to install '\
+		"$1" && sudo yum install "$1" & return
+	echo "I can't auto-install $1- Please install it and then run this "
+	echo "script again."
+	exit
+}
+__need () {
+	hash "$1" && echo "$1 is installed... " && return
+	__install "$1"
+}
+
+
+
+__need git
+__need zsh
+
+
